ae29d699f4e858d76f31738df86d278a13a48bb1
David Banda <davidbandag96@gmail.com>
2020-11-22 19:03:45 -0700

    store in a file the original logistic regression funciont to the test images persons_nonpersons

---
+++ ai_backend/neural_network/logistic_regression_test.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from ai_backend.utils.lr_utils import load_dataset
+from ai_backend.neural_network.initialize_with_zeros import initialize_with_zeros
+from ai_backend.neural_network.propagate import propagate
+from ai_backend.neural_network.optimize import optimize
+from ai_backend.neural_network.model import model
+
+# Loading the data (person/non-person)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+dim = 2
+w, b = initialize_with_zeros(dim)
+
+w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1, 2], [3, 4]]), np.array([[1, 0]])
+grads, cost = propagate(w, b, X, Y)
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=False)
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=3000, learning_rate=0.005, print_cost=True)
+
+# Example of a picture that was wrongly classified.
+index = 85
+plt.imshow(test_set_x[:, index].reshape((num_px, num_px, 3)))
+print("y = " + str(test_set_y[0, index]) + ", you predicted that it is a \"" + classes[int(d["Y_prediction_test"][0, index])].decode("utf-8") + "\" picture.")
