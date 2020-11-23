4706f13024d9a5d9d16cce00286ac95e3595727b
David Banda <davidbandag96@gmail.com>
2020-11-22 19:04:23 -0700

    add another function to process the frame and get if theres a person or not

--- ai_backend/video_stream/video_camera.py
+++ ai_backend/video_stream/video_camera.py
@@ -1,8 +1,10 @@
 import cv2
+import numpy as np
 
 
 class VideoCamera:
     def __init__(self):
+        # self.video = cv2.VideoCapture('http://50.197.223.170:80/mjpg/video.mjpg')
         self.video = cv2.VideoCapture(0)
 
     def __del__(self):
@@ -12,3 +14,13 @@ class VideoCamera:
         ret, image = self.video.read()
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
+
+    def detect_person(self):
+        ret, image = self.video.read()
+        ret, image = cv2.imencode('.jpg', image)
+        image = cv2.resize(image, (64, 64), interpolation=cv2.INTER_CUBIC)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = np.array(image[None])
+        image = image.reshape(image.shape[0], -1).T
+        image = image / 255.
+        return image
