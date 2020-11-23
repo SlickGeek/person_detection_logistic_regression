5911f123c0e06d2e010eeaa7eb12aa382cfe6d58
David Banda <davidbandag96@gmail.com>
2020-11-22 19:49:06 -0700

    add predict object to the routes file

--- ai_backend/video_stream/routes.py
+++ ai_backend/video_stream/routes.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, Response
 from ai_backend.video_stream.video_camera import VideoCamera
+from ai_backend.neural_network.predict_image import PredictImage
 
 video_stream = Blueprint('video_stream', __name__)
-
+predict = PredictImage()
 
 def gen(camera):
     while True:
         frame = camera.get_frame()
         frame_person = camera.detect_person()
-        # print(predict_image.predict_image(frame_person))
+        print(predict.predict_image(frame_person))
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
