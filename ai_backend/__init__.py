fbadb9cdac87547358f8f2e4db2f40649bf209c1
David Banda <davidbandag96@gmail.com>
2020-12-02 20:47:39 -0700

    comment the neural network

--- ai_backend/__init__.py
+++ ai_backend/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
-from ai_backend.neural_network.predict_image import PredictImage
+# from ai_backend.neural_network.predict_image import PredictImage
 
 app = Flask(__name__)
 ma = Marshmallow(app)
-predict = PredictImage()
+# predict = PredictImage()
 
 # Register Blueprint video_stream
 from ai_backend.video_stream.routes import video_stream
--- ai_backend/video_stream/routes.py
+++ ai_backend/video_stream/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, Response, request, jsonify
 from ai_backend.video_stream.video_camera import VideoCamera
 import cv2
-from ai_backend import predict
+# from ai_backend import predict
 
 video_stream = Blueprint('video_stream', __name__)
 
@@ -10,19 +10,19 @@ def gen_frame(camera):
     while True:
         jpeg, frame = camera.get_frame()
 
-        if int(predict.predict_image(frame)) == 1:
-            label_image = 'Persona detectada'
-            color = (31, 31, 234)
-        else:
-            color = (23, 211, 23)
-            label_image = 'Persona no detectada'
-        # print(predict.predict_image(frame))
-
-        # process and add text to the image
-        height, width, channels = jpeg.shape
-        # print(height, width)
-        cv2.putText(jpeg, label_image, (int(width * .03), int(height * .9)), cv2.FONT_HERSHEY_SIMPLEX, 2,
-                    color, 3)
+        # if int(predict.predict_image(frame)) == 1:
+        #     label_image = 'Persona detectada'
+        #     color = (31, 31, 234)
+        # else:
+        #     color = (23, 211, 23)
+        #     label_image = 'Persona no detectada'
+        # # print(predict.predict_image(frame))
+        #
+        # # process and add text to the image
+        # height, width, channels = jpeg.shape
+        # # print(height, width)
+        # cv2.putText(jpeg, label_image, (int(width * .03), int(height * .9)), cv2.FONT_HERSHEY_SIMPLEX, 2,
+        #             color, 3)
         ret, jpeg = cv2.imencode('.jpg', jpeg)
 
         yield (b'--frame\r\n'
